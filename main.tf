78dfaa01c7ba9675b75222b7e43130f0c59cb334
crokenx <quesadadiaz18@gmail.com>
2024-11-09 13:31:49 -0500

    fix: blank line deleted

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ module "vpc" {
   subnet_a_cidr      = "10.0.1.0/24"
   subnet_b_cidr      = "10.0.2.0/24"
   availability_zones = ["us-east-1a", "us-east-1b"]
-
 }
 
 # Implementación del módulo IGW y Routing
